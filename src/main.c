6c79511ed3d5ca187a94184fcddd416462c6c90e
Krystian <ratajczyk.krystian7@gmail.com>
2026-04-19 23:59:30 +0200

    style: zmiany w estetyce

--- src/main.c
+++ src/main.c
@@ -19,37 +19,33 @@ int main(int argc, char **argv) {
     char *format = "txt";
     char *algorithm = NULL;
     int verbose = 0;
-    
-    while ((opt = getopt(argc, argv, "vhf:o:a:")) != -1) {
+
+    while ((opt = getopt(argc, argv, "vhf:o:a:i:")) != -1) {
         switch (opt) {
         case 'v': verbose = 1; break;
         case 'h': help(); return 0;
         case 'f': format = optarg; break;
         case 'o': output = optarg; break;
         case 'a': algorithm = optarg; break;
+        case 'i': input = optarg; break;
         default: help(); return 1;
         }
-    }
-    if (optind < argc) {
-        input = argv[optind];
-    } else {
-        fprintf(stderr, "Blad: Nie podano pliku wejsciowego\n");
-        help();
-        return 1;
     }
 
     if (verbose) printf("Info: Tryb verbose wlaczony\n");
     if (verbose) printf("Info: Wczytuje graf z pliku '%s'\n", input);
+
+    if (!input) printf("Blad 1: Nie podano pliku wejsciowego\n");
     FILE *input_file = fopen(input, "r");
     if (!input_file) {
-        fprintf(stderr, "Blad: Nie mozna otworzyc pliku '%s'\n", input);
+        fprintf(stderr, "Blad 2: Nie mozna otworzyc pliku '%s'\n", input);
         return EXIT_FAILURE;
     }
     graph_t *graph = init_graph();
     load_graph(graph, input_file);
 
     if (algorithm == NULL) {
-        fprintf(stderr, "Blad: Nie podano nazwy algorytmu\n");
+        fprintf(stderr, "Blad 3: Nie podano nazwy algorytmu\n");
         return EXIT_FAILURE;
     }
     if (!strcmp(algorithm, "fruch_rein")) {
@@ -59,7 +55,7 @@ int main(int argc, char **argv) {
     } else if (!strcmp(algorithm, "tutte")) {
         tutte(graph, verbose);
     } else {
-        fprintf(stderr, "Blad: '%s' nie jest obslugiwanym algorytmem\n", algorithm);
+        fprintf(stderr, "Blad 4: '%s' nie jest obslugiwanym algorytmem\n", algorithm);
         return EXIT_FAILURE;
     }
 
@@ -67,7 +63,7 @@ int main(int argc, char **argv) {
         printf("Info: Zapisuje graf do pliku '%s'\n", output);
         output_file = fopen(output, !strcmp(format, "txt") ? "w" : "wb");
         if (!output_file) {
-            fprintf(stderr, "Blad: Nie mozna otworzyc pliku wyjściowego");
+            fprintf(stderr, "Blad 5: Nie mozna otworzyc pliku wyjściowego");
             return EXIT_FAILURE;
         }
     } else {
