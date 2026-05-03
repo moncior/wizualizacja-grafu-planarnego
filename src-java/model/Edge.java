ade596751a32147c8dc0b621831fb41f0b3d8527
KarolMuniak <01206798@pw.edu.pl>
2026-05-03 21:10:48 +0200

    edycja Node i Edge

--- src-java/model/Edge.java
+++ src-java/model/Edge.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Edge {
+    public String edgeName;
+    public int fromId;
+    public int toId;
+    public double weight;
+
+    public Edge(String edgeName, int fromId, int toId, double weight) {
+        this.edgeName = edgeName;
+        this.fromId = fromId;
+        this.toId = toId;
+        this.weight = weight;
+    }
 }
--- src-java/model/Node.java
+++ src-java/model/Node.java
@@ -1,4 +1,13 @@
 package model;
 
 public class Node {
+    public int id;
+    public double x;
+    public double y;
+
+    public Node(int id, double x, double y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
 }
