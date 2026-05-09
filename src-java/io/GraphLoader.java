7976519e776601673b2c5776529ae072a5894c41
KarolMuniak <01206798@pw.edu.pl>
2026-05-09 14:04:31 +0200

    edycja GraphLoader.java

--- src-java/io/GraphLoader.java
+++ src-java/io/GraphLoader.java
@@ -1,4 +1,13 @@
 package io;
+import model.Graph;
+import java.io.IOException;
 
 public class GraphLoader {
+    public void loadEdges(String path, Graph graph) throws IOException {
+    }
+
+    public void loadCoordinates(String path, Graph graph) throws IOException {
+    }
+
+
 }
