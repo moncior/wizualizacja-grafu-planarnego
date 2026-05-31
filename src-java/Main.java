0fedd18242377ff3b669ed9a1c0c0c79d3c004bd
moncior <olioziemblewski@gmail.com>
2026-05-31 12:16:34 +0200

    Pliki main i graphpanel

--- src-java/Main.java
+++ src-java/Main.java
@@ -1,26 +1,103 @@
 import view.GraphPanel;
+import model.Graph;
+import io.GraphLoader;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.File;
 
 public class Main extends JFrame {
-    private GraphPlaner graphPlaner;
 
-    public Main(){
-        setTitle("Wizualizacja Grafów - JIMP2");
+    private GraphPanel graphPanel;
+    private Graph currentGraph;
+    private GraphLoader loader;
+
+    public Main() {
+        currentGraph = new Graph();
+        loader = new GraphLoader();
+
+        setTitle("Wizualizacja Grafów");
         setSize(1000, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
-
         graphPanel = new GraphPanel();
+        graphPanel.setGraph(currentGraph);
         add(graphPanel, BorderLayout.CENTER);
-
 
         setupMenuBar();
         setupToolBar();
+    }
+
+    private void setupMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuFile = new JMenu("Plik");
+
+        JMenuItem itemLoadEdges = new JMenuItem("Wczytaj krawędzie (TXT)...");
+        itemLoadEdges.addActionListener(e -> loadFile(true));
+
+        JMenuItem itemLoadCoords = new JMenuItem("Wczytaj współrzędne (TXT)...");
+        itemLoadCoords.addActionListener(e -> loadFile(false));
+
+        JMenuItem itemExit = new JMenuItem("Zakończ");
+        itemExit.addActionListener(e -> System.exit(0));
+
+        menuFile.add(itemLoadEdges);
+        menuFile.add(itemLoadCoords);
+        menuFile.addSeparator();
+        menuFile.add(itemExit);
+
+        menuBar.add(menuFile);
+        setJMenuBar(menuBar);
+    }
+
+    private void loadFile(boolean isEdges) {
+        JFileChooser fileChooser = new JFileChooser();
+        int response = fileChooser.showOpenDialog(this);
+
+        if (response == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            try {
+                if (isEdges) {
+                    loader.loadEdges(file.getAbsolutePath(), currentGraph);
+                    JOptionPane.showMessageDialog(this, "Struktura krawędzi została poprawnie wczytana.");
+                } else {
+                    loader.loadCoordinates(file.getAbsolutePath(), currentGraph);
+                    JOptionPane.showMessageDialog(this, "Współrzędne węzłów zostały zaktualizowane.");
+                }
+                graphPanel.repaint();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(this,
+                        "Wystąpił błąd podczas wczytywania: \n" + ex.getMessage(),
+                        "Błąd odczytu", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private void setupToolBar() {
+        JPanel toolBar = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        toolBar.setBackground(new Color(230, 230, 230));
+
+        JCheckBox labelsCheckBox = new JCheckBox("Pokaż etykiety", true);
+        labelsCheckBox.setBackground(toolBar.getBackground());
+        labelsCheckBox.addActionListener(e -> graphPanel.setShowLabels(labelsCheckBox.isSelected()));
+
+        JButton resetButton = new JButton("Resetuj pozycje");
+        resetButton.addActionListener(e -> {
+            JOptionPane.showMessageDialog(this, "Wczytaj ponownie plik współrzędnych, aby zresetować układ.");
+        });
+
+        toolBar.add(labelsCheckBox);
+        toolBar.add(resetButton);
+
+        add(toolBar, BorderLayout.NORTH);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Main window = new Main();
+            window.setVisible(true);
+        });
     }
 }
--- src-java/view/GraphPanel.java
+++ src-java/view/GraphPanel.java
@@ -1,4 +1,5 @@
 package view;
+
 import model.Edge;
 import model.Graph;
 import model.Node;
@@ -8,14 +9,14 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class GraphPanel extends JPanel{
+public class GraphPanel extends JPanel {
+
     private Graph graph;
-    private Node draggedNote = null;
-    private double scale = 1.0;
+    private Node draggedNode = null;
     private boolean showLabels = true;
+    private final int NODE_RADIUS = 15;
 
     public GraphPanel() {
-
         setBackground(Color.WHITE);
 
         MouseAdapter mouseHandler = new MouseAdapter() {
@@ -24,8 +25,8 @@ public class GraphPanel extends JPanel{
                 if (graph == null) return;
 
                 for (Node node : graph.getNodes()) {
-                    double distance = Math.hypot(e.getX() - node.getX(), e.getY() - node.getY());
-                    if (distance <= 15) {
+                    double distance = Math.hypot(e.getX() - node.x, e.getY() - node.y);
+                    if (distance <= NODE_RADIUS) {
                         draggedNode = node;
                         break;
                     }
@@ -35,8 +36,8 @@ public class GraphPanel extends JPanel{
             @Override
             public void mouseDragged(MouseEvent e) {
                 if (draggedNode != null) {
-                    draggedNode.setX(e.getX());
-                    draggedNode.setY(e.getY());
+                    draggedNode.x = e.getX();
+                    draggedNode.y = e.getY();
                     repaint();
                 }
             }
@@ -50,3 +51,53 @@ public class GraphPanel extends JPanel{
         addMouseListener(mouseHandler);
         addMouseMotionListener(mouseHandler);
     }
+
+    public void setGraph(Graph graph) {
+        this.graph = graph;
+        repaint();
+    }
+
+    public void setShowLabels(boolean showLabels) {
+        this.showLabels = showLabels;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (graph == null) return;
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(Color.GRAY);
+        g2d.setStroke(new BasicStroke(2));
+        for (Edge edge : graph.getEdges()) {
+            Node source = graph.findNode(edge.fromId);
+            Node target = graph.findNode(edge.toId);
+
+            if (source != null && target != null) {
+                g2d.drawLine((int) source.x, (int) source.y, (int) target.x, (int) target.y);
+            }
+        }
+
+        int diameter = NODE_RADIUS * 2;
+        for (Node node : graph.getNodes()) {
+            int nx = (int) node.x;
+            int ny = (int) node.y;
+
+            g2d.setColor(new Color(100, 150, 255));
+            g2d.fillOval(nx - NODE_RADIUS, ny - NODE_RADIUS, diameter, diameter);
+
+            g2d.setColor(Color.BLACK);
+            g2d.setStroke(new BasicStroke(1));
+            g2d.drawOval(nx - NODE_RADIUS, ny - NODE_RADIUS, diameter, diameter);
+
+            if (showLabels) {
+                g2d.setColor(Color.BLACK);
+                g2d.drawString(String.valueOf(node.id), nx - NODE_RADIUS, ny - NODE_RADIUS - 5);
+            }
+        }
+    }
+}
